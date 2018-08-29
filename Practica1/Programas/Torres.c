561bf3e7f6cb94671273beca9334220271812b92
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-08-28 21:03:19 -0500

    Programas

--- Practica1/Programas/Torres.c
+++ Practica1/Programas/Torres.c
@@ -1,1 +1,103 @@
+/*
+	Autores: 
+
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Contador de numero de movimientos
+int v = 0;
+
+// Prototipo de funciones
+
+void hanoi(int n, int disco, int **M, int origen, int destino, int aux);
+void mover(int n, int disco, int **M, int o, int d);
+void torre(int n, int **M, int t);
+void sacar(int n, int disco, int t);
+void matriz(int n, int **M);
+
+// Funcion principal
+int main(void)
+{
+	int i, j, num, **A;
+	printf("\n Ingrese el numero de discos: ");
+	scanf("%d", &num);
+
+	// Arreglo que contiene el numero de discos	
+	A = malloc(3*sizeof(int*));
+	for(i=0; i<3; i++)
+		A[i] = calloc(n, sizeof(int));
+	for(i=0; i<n; i++)
+		A[0][i] = i + 1;
+
+	// Arma el estado inicial de las torres
+	for(i=0; i<3; i++)
+		torre(n, A, i);
+
+	// Llamada a la función que resuelve el caso de n discos
+	hanoi(n, n, A, 0, 2, 1);
+
+	printf("\n Movimientos realizados %d \n", v);
+	return 0;
+}
+
+/*
+	Resuelve el problema de hanoi de forma recursiva, 
+	recibe el numero total de discos, el numero de 
+	discos que solucionara, la matriz en la que se 
+	guardan los numeros de los discos, la torre donde 
+	estan los discos, la torre a donde se quiere mover 
+	los disco y la torre auxiliar.
+*/
+void hanoi(int n, int disco, int **M, int origen, int destino, int aux);
+{
+	// Caso base: Solucion al caso de un disco.
+	if (disco == 1)
+		mover(n, disco, M, origen, destino);
+	
+	else // Caso recursivo
+	{
+		/* 
+			Se llama recursivamente la funcion bajando
+			el numero de discos a mover y cambiando el
+			destino por el auxiliar, representa el mover
+			todos los discos que hay sobre el disco base
+			para poder mover el disco base.
+		*/
+		hanoi(n, disco-1, M, origen, aux, destino);
+
+		// Mueve el disco del origen al destino
+		mover(n, disco, M, origen, destino);
+
+		/*
+			Se llama recursivamente bajando el numero de
+			discos a mover y cambiando el origen por el
+			auxiliar.
+		*/
+		hanoi(n, disco-1, M, aux, destino, origen);
+	}
+	return;
+}
+
+void mover(int n, int disco, int **M, int o, int d)
+{
+	int i = 0, y;
+
+	// Imprime el movimiento
+	printf(" %d Mover de %c a %c ", v, o+65, d+65);
+	v++;
+	// Coloca el movimiento del disco en la matriz
+	while(M[o][i]!=disco)
+		i++;
+	M[d][i] = disco;
+}
+
+
+
+
+
+
+
+
