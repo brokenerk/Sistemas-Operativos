64d3a28785189c9fc693b0c78798f9014a09510f
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-09-02 13:54:29 -0500

    Asteriscos corregidos

---
+++ Practica1/Programas/rombos.c
@@ -0,0 +1,145 @@
+#include<stdio.h>
+
+void archivo(int N){
+	FILE* archivo;
+	archivo=fopen("salida.txt","w"); 
+	if(archivo!=NULL)
+	{	
+		int aux,aux1, ini, fin;
+		if(N%2==0)
+			N+=1;
+		aux1=N;
+		aux=aux1;
+		ini=N/2;
+		fin=ini;
+		for(int i=0;i<=aux/2;i++){
+			for (int j=0;j<aux;j++){
+				if (j==ini || j==fin){
+					fprintf(archivo, "*");
+				}
+				else{
+					fprintf(archivo, " ");
+				}
+			}
+			for(int j=1;j<=aux;j++){
+				if (j>=ini+1 && j<=fin+1){
+						fprintf(archivo, "*");
+				}
+				else{
+					fprintf(archivo, " ");
+				}
+			}
+			ini--;
+			fin++;
+			fprintf(archivo, "\n");
+		}
+		for(int i=0;i<2*aux;i++){
+			fprintf(archivo, "*");
+		}
+		ini++;
+		fin--;
+		fprintf(archivo, "\n");
+		for(int i=0;i<=(aux/2);i++){
+			for (int j=0;j<aux;j++){
+				if (j==ini || j==fin){
+					fprintf(archivo, "*");
+				}
+				else{
+					fprintf(archivo, " ");
+				}
+			}
+			for(int j=1;j<=aux;j++){
+				if (j>=ini+1 && j<=fin+1){
+						fprintf(archivo, "*");
+				}
+				else{
+					fprintf(archivo, " ");
+				}
+			}
+			ini++;
+			fin--;
+			fprintf(archivo, "\n");
+		}
+		fclose(archivo);
+		printf("\nArchivo guardado como 'salida.txt'");
+	}
+	else
+		printf("\nError");
+
+}
+
+void asteriscos(int N){
+	int aux,aux1, ini, fin;
+	if(N%2==0)
+		N+=1;
+	aux1=N;
+	aux=aux1;
+	ini=N/2;
+	fin=ini;
+	for(int i=0;i<=aux/2;i++){
+		for (int j=0;j<aux;j++){
+			if (j==ini || j==fin){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		for(int j=1;j<=aux;j++){
+			if (j>=ini+1 && j<=fin+1){
+					printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		ini--;
+		fin++;
+		printf("\n");
+	}
+	for(int i=0;i<2*aux;i++){
+		printf("*");
+	}
+	ini++;
+	fin--;
+	printf("\n");
+	for(int i=0;i<=(aux/2);i++){
+		for (int j=0;j<aux;j++){
+			if (j==ini || j==fin){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		for(int j=1;j<=aux;j++){
+			if (j>=ini+1 && j<=fin+1){
+					printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		ini++;
+		fin--;
+		printf("\n");
+	}
+}
+
+int main (){
+	int N, resp;
+
+	printf("Ingresa el numero de asteriscos que tendra: ");
+	scanf("%d", &N);
+
+	do{
+		printf("Imposible dibujar. Ingrese otro numero: ");
+		scanf("%d", &N);
+	}while(N%2 || !((N/2)%2));
+	
+	asteriscos(N/2);
+	printf("\nGuardar en archivo? Si=1/No=0: ");
+		scanf("%d",&resp);
+		if(resp==1)
+			archivo(N/2);
+}
