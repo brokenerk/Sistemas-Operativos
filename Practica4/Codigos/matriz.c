51f4d508bd288fbb62ff9d9ce87f4281ba7f1ba9
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-10-16 17:52:16 -0500

    Matriz
    Suma, Resta, Multiplica, Transpuesta

--- Practica4/Codigos/matriz.c
+++ Practica4/Codigos/matriz.c
@@ -1,61 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define n 3
+
+// Declaracion de funciones
+void Imprimir(int m[][n]);
+void sumar(int m1[][n], int m2[][n], int resultado[][n]);
+void restar(int m1[][n], int m2[][n], int resultado[][n]);
+void multiplicar(int m1[][n], int m2[][n], int resultado[][n]);
+void transpuesta(int m[][n], int resultado[][n]);
+
 
 int main(int argc, char const *argv[])
 {
-	int i, j, k, aux, n = 3;
-	//int matriz1[n][n], matriz2[n][n];
+	int i, j, k, aux;
 	int suma[n][n], resta[n][n], multiplica[n][n], trans[n][n], inversa[n][n];
 	
-	int matriz1[3][3] = {{2, 0, 1}, {3, 0, 0}, {5, 1, 1}};
-	int matriz2[3][3] = {{1, 0, 1}, {1, 2, 1}, {1, 1, 0}};	
+	int matriz1[n][n] = {{2, 0, 1}, {3, 0, 0}, {5, 1, 1}};
+	int matriz2[n][n] = {{1, 0, 1}, {1, 2, 1}, {1, 1, 0}};	
 
-	// Suma
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < n; j++)
-		{
-			suma[i][j] = matriz1[i][j] + matriz2[i][j]; 			
-		}
-	}
+	printf("MATRIZ 1\n");
+	Imprimir(matriz1);
 
-	// Imprimir matriz
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < n; j++)
-		{
+	printf("MATRIZ 2\n");
+	Imprimir(matriz2);
+
+	sumar(matriz1, matriz2, suma);
+	printf("SUMA:\n");
+	Imprimir(suma);
+
+
+	restar(matriz1, matriz2, resta);
+	printf("RESTA:\n");
+	Imprimir(resta);
+
+	multiplicar(matriz1, matriz2, multiplica);
+	printf("MULTIPLICACION\n");
+	Imprimir(multiplica);
+
+	transpuesta(matriz1, trans);
+	printf("TRANSPUESTA\n");
+	Imprimir(trans);
+
 	
-			printf(" %d/n",suma[i][j]);					
-		}
-	}
+	return 0;
+}
 
-	// Resta
+/****************************************************************
+							FUNCIONES
+/****************************************************************/
+void Imprimir(int m[][n])
+{
+	int aux;
+	int i, j;
 	for(i = 0; i < n; i++)
 	{
 		for(j = 0; j < n; j++)
 		{
-			suma[i][j] = matriz1[i][j] + matriz2[i][j]; 			
+			printf(" %d, ", m[i][j]);					
+			if(j == 2)
+			{
+				printf("\n");
+			}
 		}
 	}
+	printf("\n");
+}
 
-	// Multiplicacion
+void sumar(int m1[][n], int m2[][n], int resultado[][n])
+{
+	int i, j;
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < n; j++)
+		{
+			resultado[i][j] = m1[i][j] + m2[i][j]; 			
+		}
+	}
+}
 
+
+void restar(int m1[][n], int m2[][n], int resultado[][n])
+{
+	int i, j;
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < n; j++)
+		{
+			resultado[i][j] = m1[i][j] - m2[i][j]; 			
+		}
+	}
+}
+void multiplicar(int m1[][n], int m2[][n], int resultado[][n])
+{
+	int i, j, k, aux;
 	for(i = 0; i < n; i++)
 	{
 		for(j = 0; j < n; j++)
 		{
 			aux = 0;
-			for(k = 0; k < n; j++)
+			for(k = 0; k < n; k++)
 			{
-				aux = matriz1[i][k] * matriz2[k][j] + aux;
+				aux = m1[i][k] * m2[k][j] + aux;
 			}
-			multiplica[i][j] = aux;
+			resultado[i][j] = aux;
 		}
 	}
+}
 
-	// Transpuesta
-
-	
-
-		return 0;
+void transpuesta(int m[][n], int resultado[][n])
+{
+	int i, j;
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < n; j++)
+		{
+			resultado[i][j] = m[j][i];
+		}
+	}
 }
