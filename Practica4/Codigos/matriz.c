f8f6dfc13fb05594efd01e831096786164401e0b
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-10-16 19:53:14 -0500

    ...

--- Practica4/Codigos/matriz.c
+++ Practica4/Codigos/matriz.c
@@ -8,7 +8,8 @@ void sumar(int m1[][n], int m2[][n], int resultado[][n]);
 void restar(int m1[][n], int m2[][n], int resultado[][n]);
 void multiplicar(int m1[][n], int m2[][n], int resultado[][n]);
 void transpuesta(int m[][n], int resultado[][n]);
-int determinante(int m[][n], int len);
+int determinante(int m[][n]);
+void inversa(int matriz1[][n]);
 
 
 int main(int argc, char const *argv[])
@@ -16,10 +17,10 @@ int main(int argc, char const *argv[])
 	int i, j, k, aux;
 	int suma[n][n], resta[n][n], multiplica[n][n], trans[n][n], inversa[n][n];
 	
-	int matriz1[n][n] = {{2, 0, 1}, {3, 0, 0}, {5, 1, 1}};
+	int m[n][n] = {{2, 0, 1}, {3, 0, 0}, {5, 1, 1}};
 	int matriz2[n][n] = {{1, 0, 1}, {1, 2, 1}, {1, 1, 0}};	
 
-	printf("MATRIZ 1\n");
+	/*printf("MATRIZ 1\n");
 	Imprimir(matriz1);
 
 	printf("MATRIZ 2\n");
@@ -42,9 +43,12 @@ int main(int argc, char const *argv[])
 	printf("TRANSPUESTA\n");
 	Imprimir(trans);
 
-	int deter = determinante(matriz1, n);
+	int deter = determinante(matriz1);
 	printf("DETERMINANTE = %d\n", deter);
-	
+	*/
+
+	inversa(m);
+
 	return 0;
 }
 
@@ -122,28 +126,184 @@ void transpuesta(int m[][n], int resultado[][n])
 	}
 }
 
-int determinante(int m[][n], int len)
+int determinante(int m[][n])
 {
 	int i, j, producto1, producto2, col, fil, sumapos, sumaneg, suma;
 	sumapos = 0;
 	sumaneg = 0;
 	suma = 0;
-	for(i = 0; i < len; i++)
+	for(i = 0; i < n; i++)
 	{
 		producto1 = 1;
 		producto2 = 1;
-		for(j = 0; j < len; j++)
+		for(j = 0; j < n; j++)
 		{
 			col = j + i;
-			if( col >= len )
-				col = col - len;
+			if( col >= n )
+				col = col - n;
 
 			producto1 = producto1 * m[j][col];
-			producto2 = producto2 * m[len-(j+1)][col];
+			producto2 = producto2 * m[n-(j+1)][col];
 		}
 		sumapos = sumapos + producto1;
 		sumaneg = sumaneg + producto2;
 	}
 	suma = sumapos - sumaneg;
 	return suma;
+}
+
+void inversa(int matriz1[][n])
+{
+	int i, j, k, z, determ;
+	int trans[n][n];
+
+	determ = determinante(matriz1);
+
+	if(determ == 0)
+		printf("La matriz inversa no existe\n");
+	else
+	{
+		float a[10][10],resulta[10][10],identidad[10][10];
+	   	 float s=0,q;
+	   	   	 
+	   	 for(i=0;i<10;i++)
+	   	 {
+	   		 for(j=0;j<10;j++)
+	   		 {
+	   			 a[i][j]=(float)matriz1[i][j];
+	   			 if(j==i)
+	   				 identidad[i][j]=1;
+	   			 else
+	   				 identidad[i][j]=0;
+	   		 }
+	   	 }
+	   	 
+	   	 printf("\nMatriz A:\n");
+	   	 for(i=0;i<10;i++)
+	   	 {
+	   		 for(j=0;j<10;j++)
+	   		 {
+	   			 printf("%.2f  ",a[i][j]);
+	   		 }
+	   		 printf("\n");
+	   	 }
+	   	 printf("\nMatriz I:\n");
+	   	 for(i=0;i<10;i++)
+	   	 {
+	   		 for(j=0;j<10;j++)
+	   		 {
+	   			 printf("%.2f  ",identidad[i][j]);
+	   		 }
+	   		 printf("\n");
+	   	 }
+	   	 for(k=0;k<10;k++)
+	   	 {
+	   		 for(i=0;i<10;i++)
+	   		 {
+	   			 for(j=0;j<10;j++)
+	   			 {
+	   				 resulta[i][j]=0;
+	   			 }
+	   		 }
+	   		 for(i=0;i<10;i++)
+	   		 {
+	   			 for(j=0;j<10;j++)
+	   			 {
+	   				 for(z=0;z<10;z++)
+	   				 {
+	   					 resulta[i][j]+=(a[i][z]*identidad[z][j]);
+	   				 }
+	   			 }
+	   		 }
+	   		 printf("\nMatriz C(%d):\n",k);
+	   		 for(i=0;i<10;i++)
+	   		 {
+	   			 for(j=0;j<10;j++)
+	   			 {
+	   				 printf("%.2f  ",resulta[i][j]);
+	   			 }
+	   			 printf("\n");
+	   		 }
+	    
+	   		 for(i=0;i<10;i++)
+	   		 {
+	   			 for(j=0;j<10;j++)
+	   			 {
+	   				 if(i==j)
+	   				 {
+
+	   					 s+=resulta[i][j];
+	   				 }
+	   			 }
+	   		 }
+	   		 q=s/k;
+	   	 
+	   		 for(i=0;i<10;i++)
+	   		 {
+	   			 for(j=0;j<10;j++)
+	   			 {
+	   				 if(i==j)
+	   				 {
+
+	   					 identidad[i][j]=resulta[i][j]-q;
+	   				 }
+	   				 else
+	   						 identidad[i][j]=resulta[i][j];
+	   			 }
+	   		 }    
+	   		 s=0;
+	   		 
+	   	 }
+	    
+	   	 for(i=0;i<10;i++)
+	   	 {
+	   		 for(j=0;j<10;j++)
+	   		 {
+	   			 resulta[i][j]=0;
+	   		 }
+	   	 }
+	   	 for(i=0;i<10;i++)
+	   	 {
+	   		 for(j=0;j<10;j++)
+	   		 {
+	   			 for(z=0;z<10;z++)
+	   			 {
+	   				 resulta[i][j]+=(a[i][z]*identidad[z][j]);
+	   			 }
+	   		 }
+	   	 }
+
+	   	 for(i=0;i<10;i++)
+	   	 {
+	   		 for(j=0;j<10;j++)
+	   		 {
+	   			 if(i==j)
+	   				 s+=resulta[i][j];
+	   		 }
+	   	 }
+	   	 if(s/10!=0)
+	   	 {
+	   		 for(i=0;i<10;i++)
+	   		 {
+	   			 for(j=0;j<10;j++)
+	   			 {
+	   				 identidad[i][j]=identidad[i][j]/(float)(s/10);
+	   			 }
+	   		 }    
+	   	 }
+	   	 printf("\nresult:\n");
+	   	 for(i=0;i<10;i++)
+	   	 {
+	   		 for(j=0;j<10;j++)
+	   		 {
+	   			 printf("%d\t",(int)identidad[i][j]);
+	   		 }
+	   		 printf("\n");
+	   	 }
+	   	 
+
+
+	}
+
+
 }
