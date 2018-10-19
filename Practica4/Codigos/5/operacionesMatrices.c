88423d97d657bf58676f96a77e85d99c5c4ec286
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-10-18 23:16:06 -0500

    Revisa si una matriz tiene inversa
    Con ayuda del determinante, si es cero, no se hace el algoritmo.

--- Practica4/Codigos/5/operacionesMatrices.c
+++ Practica4/Codigos/5/operacionesMatrices.c
@@ -1,3 +1,5 @@
+//Compilar Linux: gcc operacionesMatrices.c -lm -o operacionesMatrices
+//Ejecutar: ./operacionesMatrices
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,16 +13,16 @@ void sumar(double **m1, double **m2, double **resultado, int n);
 void restar(double **m1, double **m2, double **resultado, int n);
 void multiplicar(double **m1, double **m2, double **resultado, int n);
 void transpuesta(double **m, double **resultado, int n);
-void inversa(double **matriz, double **resultado, int n);
+int inversa(double **matriz, double **resultado, int n);
+double determinante(double **matriz, int n);
+
 int main(int argc, char const *argv[])
 {
-	int i, j, k, l, m, n, o, p;
-	int det;
-	double **matriz1, **matriz2, **suma, **resta, **mul, **tran, **inv;
+	int i, n;
+	double **matriz1, **matriz2, **suma, **resta, **mul, **tran1, **tran2, **inv1, **inv2;
 	time_t t;
 	srand((unsigned) time(&t));
-	srand((unsigned)time(&t));
-	n = 3;
+	n = 10;
 
 	// Inicializa las matrices.
 	matriz1 = (double**)calloc(n,sizeof(double*));
@@ -43,17 +45,25 @@ int main(int argc, char const *argv[])
 	for (i = 0; i < n; i++)
 		mul[i] = (double*)calloc(n,sizeof(double));
 
-	tran = (double**)calloc(n,sizeof(double*));
+	tran1 = (double**)calloc(n,sizeof(double*));
 	for (i = 0; i < n; i++)
-		tran[i] = (double*)calloc(n,sizeof(double));
+		tran1[i] = (double*)calloc(n,sizeof(double));
+
+	tran2 = (double**)calloc(n,sizeof(double*));
+	for (i = 0; i < n; i++)
+		tran2[i] = (double*)calloc(n,sizeof(double));
 	
-	inv = (double**)calloc(n,sizeof(double*));
+	inv1 = (double**)calloc(n,sizeof(double*));
 	for (i = 0; i < n; i++)
-		inv[i] = (double*)calloc(n,sizeof(double));
+		inv1[i] = (double*)calloc(n,sizeof(double));
+
+	inv2 = (double**)calloc(n,sizeof(double*));
+	for (i = 0; i < n; i++)
+		inv2[i] = (double*)calloc(n,sizeof(double));
 
 	// Llena matriz 1 y matriz 2
 	llenar(matriz1, n);
-	llenar(matriz2,  n);
+	llenar(matriz2, n);
 
 	printf("MATRIZ 1\n"); imprimir(matriz1, n);
 	printf("MATRIZ 2\n"); imprimir(matriz2, n);
@@ -61,8 +71,17 @@ int main(int argc, char const *argv[])
 	printf("SUMA\n"); sumar(matriz1, matriz2, suma, n); imprimir(suma, n);
 	printf("RESTA\n"); restar(matriz1, matriz2, resta, n); imprimir(resta, n);
 	printf("MULTIPLICAR\n"); multiplicar(matriz1, matriz2, mul, n); imprimir(mul, n);
-	printf("TRANSPUESTA\n"); transpuesta(matriz1, tran, n); imprimir(tran, n);
-	printf("INVERSA\n"); inversa(matriz1, inv, n); imprimir(inv, n);
+	printf("TRANSPUESTA MATRIZ 1\n"); transpuesta(matriz1, tran1, n); imprimir(tran1, n);
+	printf("TRANSPUESTA MATRIZ 2\n"); transpuesta(matriz2, tran2, n); imprimir(tran2, n);
+
+	printf("INVERSA MATRIZ 1\n"); 
+	//Revisamos si la maztriz tiene inversa
+	if(inversa(matriz1, inv1, n) != 0) 
+		imprimir(inv1, n);
+
+	printf("INVERSA MATRIZ 2\n"); 
+	if(inversa(matriz2, inv2, n) != 0) 
+		imprimir(inv2, n);
 	return 0;
 }
 
@@ -72,7 +91,7 @@ void imprimir(double **m, int n)
 	for(i = 0; i < n; i++)
 	{
 		for(j = 0; j < n; j++)
-			printf(" %g, ", m[i][j]);					
+			printf("%.3f\t", m[i][j]);					
 		printf("\n");
 	}
 	printf("\n");
@@ -81,13 +100,12 @@ void imprimir(double **m, int n)
 // Llena con numeros random 
 void llenar(double **m, int n)
 {
-	int i, j, l = 1;
+	int i, j;
 	for (i = 0; i < n; i++) 
 	{
 		for (j = 0; j < n; j++)
 		 {
-			m[i][j] = (rand()%10);
-			//m[i][j] = l++;
+			m[i][j] = (rand()%11);
 		}
 	}
 }
@@ -138,50 +156,103 @@ bool esCero(double x)
 {
 	return fabs(x) < 1e-8;
 }
-// Usando definicion de la adjunta
-void inversa(double **A, double **resultado, int n)
-{
-	int i, j, k, l; 
-	double *tmp;
-	tmp = (double*)malloc(sizeof(double)*n);
 
-	for(i = 0; i < n; ++i)
-		resultado[i][i] = 1;
-	i = 0; j = 0;
-	while(i < n && j < n)
+double determinante(double **m, int n)
+{  
+	double det = 0, aux = 0;
+    int c;
+    //Si el orden es de 2, multiplica cruzadon directamente
+    if(n==2)
+        return m[0][0]*m[1][1] - m[1][0]*m[0][1];
+    else
 	{
-		if(esCero(A[i][j]))
+		for(int j=0; j<n; j++)
 		{
-			for(k = i + 1; k < n; ++k)
+			//Crea arreglo dinamico temporal
+			double **menor = (double **)malloc(sizeof(double)*(n-1));
+			//Redimensiona
+            for(int i=0; i<(n-1); i++) 
+				menor[i] = (double *)malloc(sizeof(double)*(n-1));
+            for(int k=1; k<n; k++)
 			{
-				if(!esCero(A[k][j]))
+                c = 0;
+                    for(int l=0; l<n; l++)
+					{
+                        if(l!=j)
+						{
+							/*Parte matriz principal en matrices de 3 
+							y multiplica cruzado*/
+                			menor[k-1][c] = m[k][l];
+                            c++;
+                        }
+                    }
+            }
+        //Recursividad, repite la funcion
+        aux = pow(-1, 2+j)*m[0][j]*determinante(menor, n-1);
+        det += aux;
+
+        for(int x = 0; x<(n-1); x++)
+            free(menor[x]);//Libera espacio en memoria     
+        free(menor);
+        }
+    return det;//Devuelve resultado
+    }
+}
+
+// Usando definicion de la adjunta
+int inversa(double **A, double **resultado, int n)
+{
+	int tieneInversa;
+	if(determinante(A, n) == 0)
+	{
+		tieneInversa=0;
+		printf("La matriz no tiene inversa. Determinante = 0\n\n");
+	}
+	else{
+		tieneInversa=1;
+		int i, j, k, l; 
+		double *tmp;
+		tmp = (double*)malloc(sizeof(double)*n);
+
+		for(i = 0; i < n; ++i)
+			resultado[i][i] = 1;
+		i = 0; j = 0;
+		while(i < n && j < n)
+		{
+			if(esCero(A[i][j]))
+			{
+				for(k = i + 1; k < n; ++k)
 				{
-					tmp = A[i];
-					A[i] = A[k];
-					A[k] = tmp;
-					tmp = resultado[i];
-					resultado[i] = resultado[k];
-					resultado[k] = tmp;
-					break;
+					if(!esCero(A[k][j]))
+					{
+						tmp = A[i];
+						A[i] = A[k];
+						A[k] = tmp;
+						tmp = resultado[i];
+						resultado[i] = resultado[k];
+						resultado[k] = tmp;
+						break;
+					}
 				}
 			}
-		}
-		if(!esCero(A[i][j]))
-		{
-			for(l = 0; l < n; ++l)
-				resultado[i][l] /= A[i][j];
-			for(l = n - 1; l >= j; --l)
-				A[i][l] /= A[i][j];
-			for(k = 0; k < n; ++k)
+			if(!esCero(A[i][j]))
 			{
-				if(i == k) continue;
 				for(l = 0; l < n; ++l)
-					resultado[k][l] -= resultado[i][l] * A[k][j];
-				for(l = n; l >= j; --l)
-					A[k][l] -= A[i][l] * A[k][j];
+					resultado[i][l] /= A[i][j];
+				for(l = n - 1; l >= j; --l)
+					A[i][l] /= A[i][j];
+				for(k = 0; k < n; ++k)
+				{
+					if(i == k) continue;
+					for(l = 0; l < n; ++l)
+						resultado[k][l] -= resultado[i][l] * A[k][j];
+					for(l = n; l >= j; --l)
+						A[k][l] -= A[i][l] * A[k][j];
+				}
+				++i;
 			}
-			++i;
+			++j;
 		}
-		++j;
 	}
+	return tieneInversa;
 }
