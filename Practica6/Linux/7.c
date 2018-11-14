3c36718538857c0ff478b4ed1ddef4269aa87764
brokenerk <enriquebroly@hotmail.com>
2018-11-14 15:12:58 -0600

    programa 7 p6

--- Practica6/Linux/7.c
+++ Practica6/Linux/7.c
@@ -1,7 +1,3 @@
-//	Compilación:
-//	gcc tiempo.c -c
-//	gcc 5.c tiempo.o -o 5
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -36,13 +32,22 @@ int *shm, *z, c, d;
 
 int main()
 {
+	
 	// CREAR DIRECTORIO
 	// Obtenemos el directorio desde la entrada de teclado
-	char* path = leerDirectorio(); 
+	char* path = leerDirectorio();
+
+	//Llamda al sistema mkdir recibe la ruta del directorio a crear, y los permisos de escritura, lectura y ejecucion para cada tipo de usuario
+	//Retorna -1 si ocurrieron errores
+	if(mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) ==-1)
+	{
+		perror(path);
+        exit(EXIT_FAILURE);
+    }
 
 	int i, n, shmid, pid;
-	double **matriz1, **matriz2, **suma, **mul, **inv1, **inv2;
-	srand(time(NULL));
+	double **matriz1, **matriz2, **matriz3, **matriz4, **suma, **mul, **inv1, **inv2;
+	
 	n = 10; // Tam de la matriz cuadrada
 	key_t llave = 5678;
 
@@ -54,6 +59,14 @@ int main()
 	matriz2 = (double**)calloc(n,sizeof(double*));
 	for (i = 0; i < n; i++)
 		matriz2[i] = (double*)calloc(n,sizeof(double));
+
+	matriz3 = (double**)calloc(n,sizeof(double*));
+	for (i = 0; i < n; i++)
+		matriz3[i] = (double*)calloc(n,sizeof(double));
+
+	matriz4 = (double**)calloc(n,sizeof(double*));
+	for (i = 0; i < n; i++)
+		matriz4[i] = (double*)calloc(n,sizeof(double));
 	
 	suma = (double**)calloc(n,sizeof(double*));
 	for (i = 0; i < n; i++)
@@ -73,13 +86,13 @@ int main()
 
 	//---------Revisamos que se haya creado la mem compartida---------------
 
-	if(shmid = shmget(llave, TAM_MEM, IPC_CREAT | 0666) < 0)
+	if((shmid = shmget(llave, TAM_MEM, IPC_CREAT | 0666)) < 0)
 	{
 		perror("Error al obtener la memoria compartida: shmget");
 		exit(-1);
 	}
 
-	if((shm = shmat(shmid, NULL, 0)) == (int *)-1)
+	if((shm = shmat(shmid, NULL, 0)) == (int*)-1)
 	{
 		perror("Error al lanzar la memoria compartida: shmat");
 		exit(-1);
@@ -91,19 +104,19 @@ int main()
 	{
 		if((pid = fork()) == 0)
 		{
-			//------------------PROCESO HIJO: MULTIPLICACION
+			//------------------PROCESO HIJO: SUMA
 			sleep(11);
 
 			printf("------------------------------\n");
-			printf("Proceso HIJO leeyendo matriz 1\n");
-			leer(matriz1, n, 456);
+			printf("Proceso HIJO leeyendo matriz 3\n");
+			leer(matriz3, n, 789);
 			sleep(4);
 
-			printf("Proceso HIJO leeyendo matriz 2\n");
-			leer(matriz2, n, 678);
+			printf("Proceso HIJO leeyendo matriz 4\n");
+			leer(matriz4, n, 890);
 
 			printf("Proceso HIJO sumando matrices\n");
-			sumar(matriz1, matriz2, suma, n);
+			sumar(matriz3, matriz4, suma, n);
 			sleep(2);
 
 			printf("Proceso PADRE escribiendo la suma\n");
@@ -113,9 +126,15 @@ int main()
 		}
 		else
 		{
-			//------------------PROCESO PADRE: SUMA
+			//------------------PROCESO PADRE: MULTIPLICACION
 			printf("------------------------------\n");
 			sleep(1);
+
+			printf("Proceso PADRE creando matriz 3\n");
+			llenar(matriz3, n); imprimir(matriz3, n);
+
+			printf("Proceso PADRE creando matriz 4\n");
+			llenar(matriz4, n); imprimir(matriz4, n);
 
 			printf("Proceso PADRE leeyendo matriz 1\n");
 			leer(matriz1, n, 456);
@@ -132,56 +151,61 @@ int main()
 			escribir(mul, n, 654);
 			sleep(2);
 
-			printf("Proceso PADRE escribiendo matriz 1\n");
-			escribir(matriz1, n, 456);
+			printf("Proceso PADRE escribiendo matriz 3\n");
+			escribir(matriz3, n, 789);
 			sleep(2);
 
-			printf("Proceso PADRE escribiendo matriz 2\n");
-			escribir(matriz2, n, 678);
+			printf("Proceso PADRE escribiendo matriz 4\n");
+			escribir(matriz4, n, 890);
 
 			exit(0);
 		}
 	}
 	else{
 		//------------------PROCESO ABUELO: Crea matrices y lee inversas
+		printf("------------------------------\n");
+		sleep(1);
 		printf("Proceso ABUELO creando matriz 1\n");
-		llenar(matriz1, n);
+		llenar(matriz1, n); imprimir(matriz1, n);
 
 		printf("Proceso ABUELO creando matriz 2\n");
-		llenar(matriz2, n);
+		llenar(matriz2, n); imprimir(matriz2, n);
 
 		printf("Proceso ABUELO escribiendo matriz 1\n");
 		escribir(matriz1, n, 456);
 		sleep(1);
 
 		printf("Proceso ABUELO escribiendo matriz 2\n");
-		escribir(matriz1, n, 678);
-		sleep(4);
-
-		printf("Proceso ABUELO leeyendo multiplicacion\n");
-		leer(mul, n ,678);
-		sleep(12);
+		escribir(matriz2, n, 678);
+		sleep(3.5);
 
 		printf("Proceso ABUELO leeyendo suma\n");
 		leer(suma, n, 654);
 		sleep(1);
 
-		printf("Multiplicacion realizada por el Proceso PADRE:\n");
-		imprimir(mul, n);
+		printf("Proceso ABUELO leeyendo multiplicacion\n");
+		leer(mul, n, 654);
+		sleep(12);
 
 		printf("Suma realizada por el Proceso HIJO:\n");
 		imprimir(suma, n);
 
-		printf("Escribiendo archivo de inversa de multiplicacion....");
-		inversa(mul, inv1, n);
-		crearArchivo(inv1, n, "/inv_mul.txt", path);
+		printf("Escribiendo archivo TXT de inversa suma....");
+		inversa(suma, inv1, n);
+		crearArchivo(inv1, n, "/inv_suma.txt", path);
 		printf("Listo\n");
 
-		printf("Escribiendo archivo de inversa suma....");
-		inversa(suma, inv2, n);
-		crearArchivo(inv2, n, "/inv_suma.txt", path);
+
+		printf("Multiplicacion realizada por el Proceso PADRE:\n");
+		imprimir(mul, n);
+
+		printf("Escribiendo archivo TXT de inversa multiplicacion....");
+		inversa(mul, inv2, n);
+		crearArchivo(inv2, n, "/inv_mul.txt", path);
 		printf("Listo\n");
 
+
+		
 		exit(0);
 	}
 	return 0;
@@ -299,6 +323,8 @@ void imprimir(double **m, int n)
 void llenar(double **m, int n)
 {
 	int i, j;
+	time_t t;
+	srand((unsigned) time(&t));
 	for (i = 0; i < n; i++) 
 	{
 		for (j = 0; j < n; j++)
